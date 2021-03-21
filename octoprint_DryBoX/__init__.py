b5c3ae2718fb9ee11fe0fe1d9082a31afbb7f8b0
5H3LL3H5 <christianstenzel@linux.com>
2021-03-21 19:14:37 +0100

    Plugin tutorial 4/7

--- octoprint_DryBoX/__init__.py
+++ octoprint_DryBoX/__init__.py
@@ -5,9 +5,19 @@ import octoprint.plugin
 
 
 class DryBoxPlugin(octoprint.plugin.StartupPlugin,
-                   octoprint.plugin.TemplatePlugin):
+                   octoprint.plugin.TemplatePlugin,
+                   octoprint.plugin.SettingsPlugin):
     def on_after_startup(self):
-        self._logger.info("DryBoX is online!")
+        self._logger.info("DryBoX (more: %s)" % self._settings.get(["url"]))
+
+    def get_settings_defaults(self):
+        return dict(url="https://github.com/5H3LL3H5/DryBoX")
+
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False)
+        ]
 
 
 __plugin_name__ = "DryBoX"
