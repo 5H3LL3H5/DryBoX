5af2166e266ffaa0be90416817022fed423b4e11
5H3LL3H5 <christianstenzel@linux.com>
2021-03-21 18:45:14 +0100

    HelloWorld Plugin UI changes

--- octoprint_DryBoX/__init__.py
+++ octoprint_DryBoX/__init__.py
@@ -4,9 +4,10 @@ from __future__ import absolute_import, unicode_literals
 import octoprint.plugin
 
 
-class DryBoxPlugin(octoprint.plugin.StartupPlugin):
+class DryBoxPlugin(octoprint.plugin.StartupPlugin,
+                   octoprint.plugin.TemplatePlugin):
     def on_after_startup(self):
-        self._logger.info("Hello World!")
+        self._logger.info("DryBoX is online!")
 
 
 __plugin_name__ = "DryBoX"
